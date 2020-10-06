6bf3d8f8022cf7a47cdea6409a9cb6f1891d0c41
Amit Sharma <sharma.amit1100@gmail.com>
2020-10-06 08:45:15 +0530

    Node Assignement

---
+++ NodeAssignment/start.js
@@ -0,0 +1,17 @@
+// var calc = require('./calc.js')
+
+// var res = calc.add(4,5);
+// console.log(res);
+
+// var fs = require('fs');
+
+// fs.readFile('calc.js','utf8', function(err, data){
+//     console.log(data);
+// });
+
+
+const express =  require('express');
+const app = express();
+
+app.listen(9000);
+
